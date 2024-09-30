8d2cf94d93e51e2ab6a1f62cdb6f21bab32bc3ee
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2024-09-29 21:16:46 -0400

    first commit

---
+++ src/routes/news.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const newsRouter = express.Router()
+const axios = require('axios')
+
+newsRouter.get('', async(req, res) => {
+    try {
+        const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?q=hurricane&q=Atlanta&apiKey=62b15376e43a4120bef86be143305846`)
+        res.render('news', { articles : newsAPI.data.articles })
+    } catch (err) {
+        if(err.response) {
+            res.render('news', { articles : null })
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        } else if(err.requiest) {
+            res.render('news', { articles : null })
+            console.log(err.requiest)
+        } else {
+            res.render('news', { articles : null })
+            console.error('Error', err.message)
+        }
+    } 
+})
+
+
+module.exports = newsRouter
